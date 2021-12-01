2fd6a5b107dac21da9d4596f1849f103d466db0b
yanchu0612 <yan.gyunashyan@gmail.com>
2021-12-01 09:24:47 +0100

    Start on code

--- src/java.rs
+++ src/java.rs
@@ -1,5 +1,8 @@
 use bytes::Buf;
 
+use crate::java::Instruction::BASIC;
+use crate::opcodes;
+
 pub struct ClassInfo {
     pub magic: u32,
 
@@ -16,7 +19,6 @@ pub struct ClassInfo {
     pub methods: Vec<MethodInfo>,
     pub attributes: Vec<AttributeInfo>,
 }
-
 
 //ClassFile {
 //     u32             magic;
@@ -46,7 +48,7 @@ pub fn get_class_info(reader: &mut &[u8]) -> ClassInfo {
     let mut constant_pool: Vec<ConstantInfo> = Vec::with_capacity(constant_pool_size);
     for i in 0..constant_pool_size {
         constant_pool.insert(i, get_constant_info(i, reader));
-    };
+    }
 
     let access_flags = reader.get_u16();
     let this_class = reader.get_u16();
@@ -56,25 +58,25 @@ pub fn get_class_info(reader: &mut &[u8]) -> ClassInfo {
     let mut interfaces: Vec<u16> = Vec::with_capacity(interfaces_size);
     for i in 0..interfaces_size {
         interfaces.insert(i, reader.get_u16());
-    };
+    }
 
     let fields_size = reader.get_u16() as usize;
     let mut fields: Vec<FieldInfo> = Vec::with_capacity(fields_size);
     for i in 0..fields_size {
         fields.insert(i, get_field(reader, &constant_pool));
-    };
+    }
 
     let methods_size = reader.get_u16() as usize;
     let mut methods: Vec<MethodInfo> = Vec::with_capacity(methods_size);
     for i in 0..methods_size {
         methods.insert(i, get_method(reader, &constant_pool));
-    };
+    }
 
     let attributes_size = reader.get_u16() as usize;
     let mut attributes: Vec<AttributeInfo> = Vec::with_capacity(attributes_size);
     for i in 0..attributes_size {
         attributes.insert(i, get_attribute_info(reader, &constant_pool));
-    };
+    }
 
     ClassInfo {
         magic,
@@ -90,7 +92,6 @@ pub fn get_class_info(reader: &mut &[u8]) -> ClassInfo {
         attributes,
     }
 }
-
 
 //field_info {
 //     u16             access_flags;
@@ -115,7 +116,7 @@ pub fn get_field(reader: &mut &[u8], constant_pool: &Vec<ConstantInfo>) -> Field
     let mut attribute_info: Vec<AttributeInfo> = Vec::with_capacity(attribute_info_size as usize);
     for _i in 0..attribute_info_size {
         attribute_info.push(get_attribute_info(reader, constant_pool));
-    };
+    }
     FieldInfo {
         access_flags,
         name_index,
@@ -147,7 +148,7 @@ pub fn get_method(reader: &mut &[u8], constant_pool: &Vec<ConstantInfo>) -> Meth
     let mut attribute_info: Vec<AttributeInfo> = Vec::with_capacity(attribute_info_size as usize);
     for _i in 0..attribute_info_size {
         attribute_info.push(get_attribute_info(reader, constant_pool));
-    };
+    }
     MethodInfo {
         access_flags,
         name_index,
@@ -157,25 +158,61 @@ pub fn get_method(reader: &mut &[u8], constant_pool: &Vec<ConstantInfo>) -> Meth
 }
 
 pub enum ConstantInfo {
-    Class { name_index: u16 },
-    Field { class_index: u16, name_and_type_index: u16 },
-    Method { class_index: u16, name_and_type_index: u16 },
-    Interface { class_index: u16, name_and_type_index: u16 },
-    String { string_index: u16 },
-    Integer { bytes: u32 },
-    Float { bytes: u32 },
-    Long { bytes: u64 },
-    Double { bytes: u64 },
-    NameAndType { name_index: u16, descriptor_index: u16 },
-    UTF8 { text: String },
-    MethodHandle { reference_kind: u8, reference_index: u16 },
-    MethodType { descriptor_index: u16 },
-    InvokeDynamic { bootstrap_method_attr_index: u16, name_and_type_index: u16 },
+    Class {
+        name_index: u16,
+    },
+    Field {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    Method {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    Interface {
+        class_index: u16,
+        name_and_type_index: u16,
+    },
+    String {
+        string_index: u16,
+    },
+    Integer {
+        bytes: u32,
+    },
+    Float {
+        bytes: u32,
+    },
+    Long {
+        bytes: u64,
+    },
+    Double {
+        bytes: u64,
+    },
+    NameAndType {
+        name_index: u16,
+        descriptor_index: u16,
+    },
+    UTF8 {
+        text: str,
+    },
+    MethodHandle {
+        reference_kind: u8,
+        reference_index: u16,
+    },
+    MethodType {
+        descriptor_index: u16,
+    },
+    InvokeDynamic {
+        bootstrap_method_attr_index: u16,
+        name_and_type_index: u16,
+    },
 }
 
 pub fn get_constant_info(i: usize, reader: &mut &[u8]) -> ConstantInfo {
     return match reader.get_u8() {
-        7 => ConstantInfo::Class { name_index: reader.get_u16() },
+        7 => ConstantInfo::Class {
+            name_index: reader.get_u16(),
+        },
         9 => ConstantInfo::Field {
             class_index: reader.get_u16(),
             name_and_type_index: reader.get_u16(),
@@ -188,11 +225,21 @@ pub fn get_constant_info(i: usize, reader: &mut &[u8]) -> ConstantInfo {
             class_index: reader.get_u16(),
             name_and_type_index: reader.get_u16(),
         },
-        8 => ConstantInfo::String { string_index: reader.get_u16() },
-        3 => ConstantInfo::Integer { bytes: reader.get_u32() },
-        4 => ConstantInfo::Float { bytes: reader.get_u32() },
-        5 => ConstantInfo::Long { bytes: reader.get_u64() },
-        6 => ConstantInfo::Double { bytes: reader.get_u64() },
+        8 => ConstantInfo::String {
+            string_index: reader.get_u16(),
+        },
+        3 => ConstantInfo::Integer {
+            bytes: reader.get_u32(),
+        },
+        4 => ConstantInfo::Float {
+            bytes: reader.get_u32(),
+        },
+        5 => ConstantInfo::Long {
+            bytes: reader.get_u64(),
+        },
+        6 => ConstantInfo::Double {
+            bytes: reader.get_u64(),
+        },
         12 => ConstantInfo::NameAndType {
             name_index: reader.get_u16(),
             descriptor_index: reader.get_u16(),
@@ -205,60 +252,324 @@ pub fn get_constant_info(i: usize, reader: &mut &[u8]) -> ConstantInfo {
             }
             let string = String::from_utf8(bytes).expect("Failed to create string.");
             println!("{}. {}", i, string);
-            ConstantInfo::UTF8 {
-                text: string
-            }
+            ConstantInfo::UTF8 { text: *string }
         }
         15 => ConstantInfo::MethodHandle {
             reference_kind: reader.get_u8(),
             reference_index: reader.get_u16(),
         },
         16 => ConstantInfo::MethodType {
-            descriptor_index: reader.get_u16()
+            descriptor_index: reader.get_u16(),
         },
         18 => ConstantInfo::InvokeDynamic {
             bootstrap_method_attr_index: reader.get_u16(),
             name_and_type_index: reader.get_u16(),
         },
-        _ => panic!("crash and burn")
+        _ => panic!("crash and burn"),
     };
 }
 
+pub struct AttributeException {
+    start_pc: u16,
+    end_pc: u16,
+    handler_pc: u16,
+    catch_type: u16,
+}
+
+pub struct AttributeClass {
+    inner_class_info_index: u16,
+    outer_class_info_index: u16,
+    inner_name_index: u16,
+    inner_class_access_flags: u16,
+}
+
+pub struct AttributeLineNumber {
+    start_pc: u16,
+    line_number: u16,
+}
+
+pub struct AttributeLocalVariable {
+    start_pc: u16,
+    length: u16,
+    name_index: u16,
+    descriptor_index: u16,
+    index: u16,
+}
+
+pub struct AttributeLocalVariableType {
+    start_pc: u16,
+    length: u16,
+    name_index: u16,
+    signature_index: u16,
+    index: u16,
+}
+
+pub struct AttributeBootstrapMethod {
+    bootstrap_method_ref: u16,
+    bootstrap_arguments: Vec<u16>,
+}
+
 pub enum AttributeInfo {
-    ConstantValue,
-    Code,
+    ConstantValue {
+        constant_index: u16,
+    },
+    Code {
+        max_stack: u16,
+        max_locals: u16,
+        code: Vec<u8>,
+        exception_table: Vec<AttributeException>,
+        attribute_info: Vec<AttributeInfo>,
+    },
+    // TODO do this
     StackMapTable,
-    Exceptions,
-    InnerClasses,
-    EnclosingMethod,
+    Exceptions {
+        exception_index_table: Vec<u16>,
+    },
+    InnerClasses {
+        classes: Vec<AttributeClass>,
+    },
+    EnclosingMethod {
+        class_index: u16,
+        method_index: u16,
+    },
     Synthetic,
-    Signature,
-    SourceFile,
-    SourceDebugExtension,
-    LineNumberTable,
-    LocalVariableTable,
-    LocalVariableTypeTable,
+    Signature {
+        signature_index: u16,
+    },
+    SourceFile {
+        source_file_index: u16,
+    },
+    SourceDebugExtension {
+        debug_extension: Vec<u8>,
+    },
+    LineNumberTable {
+        line_number_table: Vec<AttributeLineNumber>,
+    },
+    LocalVariableTable {
+        local_variable_table: Vec<AttributeLocalVariable>,
+    },
+    LocalVariableTypeTable {
+        local_variable_type_table: Vec<AttributeLocalVariableType>,
+    },
     Deprecated,
-    RuntimeVisibleAnnotations,
+    // TODO do this
     RuntimeInvisibleAnnotations,
+    // TODO do this
     RuntimeVisibleParameterAnnotations,
+    // TODO do this
     RuntimeInvisibleParameterAnnotations,
+    // TODO do this
     AnnotationDefault,
-    BootstrapMethods,
+    // TODO do this
+    RuntimeVisibleAnnotations,
+    BootstrapMethods {
+        bootstrap_methods: Vec<AttributeBootstrapMethod>,
+    },
 }
 
 pub fn get_attribute_info(reader: &mut &[u8], constant_pool: &Vec<ConstantInfo>) -> AttributeInfo {
     let attribute_name_index = reader.get_u16();
-    let x = constant_pool.get(attribute_name_index as usize - 1).expect("f");
+    let x = get_constant(attribute_name_index, constant_pool);
+    let length = reader.get_u32(); // length
     match x {
-        ConstantInfo::UTF8 { text } => { println!("Attribute Info {} {}", attribute_name_index, text); }
-        _ => { panic!("Wrong Attribute info.") }
-    };
-    let i2 = reader.get_u32();
-    for _i in 0..i2 {
-        let _i1 = reader.get_u8();
-    };
-
-    AttributeInfo::BootstrapMethods {}
+        ConstantInfo::UTF8 { text } => {
+            match text {
+                "ConstantValue" => AttributeInfo::ConstantValue {
+                    constant_index: reader.get_u16(),
+                },
+                "Code" => {
+                    let max_stack = reader.get_u16();
+                    let max_locals = reader.get_u16();
+                    let code_length = reader.get_u32();
+                    AttributeInfo::Code {
+                        max_stack,
+                        max_locals,
+                        code: vec![],
+                        exception_table: vec![],
+                        attribute_info: vec![],
+                    }
+                }
+                "StackMapTable" => {}
+                "Exceptions" => {}
+                "InnerClasses" => {}
+                "EnclosingMethod" => {}
+                "Synthetic" => {}
+                "Signature" => {}
+                "SourceFile" => {}
+                "SourceDebugExtension" => {}
+                "LineNumberTable" => {}
+                "LocalVariableTable" => {}
+                "LocalVariableTypeTable" => {}
+                "Deprecated" => {}
+                "RuntimeVisibleAnnotations" => {}
+                "RuntimeInvisibleAnnotations" => {}
+                "RuntimeVisibleParameterAnnotations" => {}
+                "RuntimeInvisibleParameterAnnotations" => {}
+                "AnnotationDefault" => {}
+                "BootstrapMethods" => {}
+                &_ => {}
+            }
+            AttributeInfo::AnnotationDefault
+        }
+        _ => {
+            for i in 0..length {
+                reader.get_u8();
+            }
+            AttributeInfo::AnnotationDefault
+        }
+    }
 }
 
+pub enum Instruction {
+    BASIC {
+        op: u8,
+    },
+    TYPED {
+        op: u8,
+        type_index: u16,
+    },
+    VAR {
+        op: u8,
+        var: u32,
+    },
+    JUMP {
+        op: u8,
+        location: u32,
+    },
+    INT {
+        op: u8,
+        int: i32,
+    },
+    FIELD {
+        op: u8,
+        owner: u16,
+        name: u16,
+        descriptor: u16,
+    },
+    METHOD {
+        op: u8,
+        owner: u16,
+        name: u16,
+        descriptor: u16,
+    },
+}
+
+pub fn get_instruction(reader: &mut &[u8]) -> Instruction {
+    let op = reader.get_u8();
+    match op {
+        opcodes::NOP
+        | opcodes::ACONST_NULL
+        | opcodes::ICONST_M1
+        | opcodes::ICONST_0
+        | opcodes::ICONST_1
+        | opcodes::ICONST_2
+        | opcodes::ICONST_3
+        | opcodes::ICONST_4
+        | opcodes::ICONST_5
+        | opcodes::LCONST_0
+        | opcodes::LCONST_1
+        | opcodes::FCONST_0
+        | opcodes::FCONST_1
+        | opcodes::FCONST_2
+        | opcodes::DCONST_0
+        | opcodes::DCONST_1
+        | opcodes::IALOAD
+        | opcodes::LALOAD
+        | opcodes::FALOAD
+        | opcodes::DALOAD
+        | opcodes::AALOAD
+        | opcodes::BALOAD
+        | opcodes::CALOAD
+        | opcodes::SALOAD
+        | opcodes::IASTORE
+        | opcodes::LASTORE
+        | opcodes::FASTORE
+        | opcodes::DASTORE
+        | opcodes::AASTORE
+        | opcodes::BASTORE
+        | opcodes::CASTORE
+        | opcodes::SASTORE
+        | opcodes::POP
+        | opcodes::POP2
+        | opcodes::DUP
+        | opcodes::DUP_X1
+        | opcodes::DUP_X2
+        | opcodes::DUP2
+        | opcodes::DUP2_X1
+        | opcodes::DUP2_X2
+        | opcodes::SWAP
+        | opcodes::IADD
+        | opcodes::LADD
+        | opcodes::FADD
+        | opcodes::DADD
+        | opcodes::ISUB
+        | opcodes::LSUB
+        | opcodes::FSUB
+        | opcodes::DSUB
+        | opcodes::IMUL
+        | opcodes::LMUL
+        | opcodes::FMUL
+        | opcodes::DMUL
+        | opcodes::IDIV
+        | opcodes::LDIV
+        | opcodes::FDIV
+        | opcodes::DDIV
+        | opcodes::IREM
+        | opcodes::LREM
+        | opcodes::FREM
+        | opcodes::DREM
+        | opcodes::INEG
+        | opcodes::LNEG
+        | opcodes::FNEG
+        | opcodes::DNEG
+        | opcodes::ISHL
+        | opcodes::LSHL
+        | opcodes::ISHR
+        | opcodes::LSHR
+        | opcodes::IUSHR
+        | opcodes::LUSHR
+        | opcodes::IAND
+        | opcodes::LAND
+        | opcodes::IOR
+        | opcodes::LOR
+        | opcodes::IXOR
+        | opcodes::LXOR
+        | opcodes::I2L
+        | opcodes::I2F
+        | opcodes::I2D
+        | opcodes::L2I
+        | opcodes::L2F
+        | opcodes::L2D
+        | opcodes::F2I
+        | opcodes::F2L
+        | opcodes::F2D
+        | opcodes::D2I
+        | opcodes::D2L
+        | opcodes::D2F
+        | opcodes::I2B
+        | opcodes::I2C
+        | opcodes::I2S
+        | opcodes::LCMP
+        | opcodes::FCMPL
+        | opcodes::FCMPG
+        | opcodes::DCMPL
+        | opcodes::DCMPG
+        | opcodes::IRETURN
+        | opcodes::LRETURN
+        | opcodes::FRETURN
+        | opcodes::DRETURN
+        | opcodes::ARETURN
+        | opcodes::RETURN
+        | opcodes::ARRAYLENGTH
+        | opcodes::ATHROW
+        | opcodes::MONITORENTER
+        | opcodes::MONITOREXIT => Instruction::BASIC { op },
+        _ => {}
+    }
+}
+
+pub fn get_constant(index: u16, constant_pool: &Vec<ConstantInfo>) -> &ConstantInfo {
+    constant_pool
+        .get(attribute_name_index as usize - 1)
+        .expect("Could not get constant. at " + index)
+}
