4843d0bb0a37a0f492da17eb6fbfbc949c9e80c4
leocth <22406910+LeoCTH@users.noreply.github.com>
2021-12-01 23:29:48 +0800

    fixed class flags

--- src/consts.rs
+++ src/consts.rs
@@ -30,6 +30,17 @@ bitflags! {
 
     pub struct ClassAccessFlags: u16 {
         const PUBLIC = 0x0001;
+        const FINAL = 0x0010;
+        const SUPER = 0x0020;
+        const INTERFACE = 0x0200;
+        const ABSTRACT = 0x0400;
+        const SYNTHETIC = 0x1000;
+        const ANNOTATION = 0x2000;
+        const ENUM = 0x4000;
+    }
+
+    pub struct InnerClassAccessFlags: u16 {
+        const PUBLIC = 0x0001;
         const PRIVATE = 0x0002;
         const PROTECTED = 0x0004;
         const STATIC = 0x0008;
